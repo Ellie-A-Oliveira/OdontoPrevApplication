d4541b383e9d3a9b0427eccdd62b8250bdc9173e
Ellie de Oliveira <ellie.a.oliveira@gmail.com>
2024-10-05 10:50:46 -0300

    feat: adiciona a entidade de rede credenciada

--- OdontoPrevApplication/Domain/Entities/RedeCredenciadaEntity.cs
+++ OdontoPrevApplication/Domain/Entities/RedeCredenciadaEntity.cs
@@ -1,6 +1,19 @@
-﻿namespace OdontoPrevApplication.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OdontoPrevApplication.Domain.Entities
 {
+    [Table("tb_rede_credenciada")]
     public class RedeCredenciadaEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public required string Nome { get; set; }
+
+        public virtual ICollection<RedeCredenciadaEntity>? RedeCredenciadas { get; set; }
     }
 }
